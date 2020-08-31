5220569134dc35f6a122a9cbd111b47142cc7402
IMHOJEONG <39ghwjd@naver.com>
2020-08-31 14:33:08 +0900

    Add Node JS API Server

---
+++ build/precache-manifest.566ccf001109f5c3dac53b7a453f935a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a078c0c373f742f74289a439e623d19c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f306330fd4bbed25447",
+    "url": "/static/css/main.d1288908.chunk.css"
+  },
+  {
+    "revision": "5d26e6822a1b8de8cfeb",
+    "url": "/static/js/2.c7b210fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c7b210fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f306330fd4bbed25447",
+    "url": "/static/js/main.2cc54304.chunk.js"
+  },
+  {
+    "revision": "49171c4d1ce88a4a8f67",
+    "url": "/static/js/runtime-main.d064a210.js"
+  },
+  {
+    "revision": "dbe0a74f4380184bfa6a4e5da9e10da0",
+    "url": "/static/media/covid.2de7ca32.png"
+  }
+]);
